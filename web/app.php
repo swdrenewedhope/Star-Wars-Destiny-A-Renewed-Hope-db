ec5fc2670887d62c34378440ee414cb846ec7831
Lewis <193742323+Draiqil@users.noreply.github.com>
2026-01-23 05:57:40 +0000

    Changed app.php so it doesn't hardcode paths.

--- web/app.php
+++ web/app.php
@@ -2,27 +2,29 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once '/home/gxpvsxmy/Servers/swdestinydb/app/bootstrap.php.cache';
+$root = realpath(__DIR__ . '/..');
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
+if (!is_file($root . '/vendor/autoload.php')) {
+    $envRoot = getenv('APP_ROOT');
+    if ($envRoot) {
+        $root = rtrim($envRoot, '/');
+    }
+}
+
+$loader = require $root . '/vendor/autoload.php';
+
 /*
+use Symfony\Component\ClassLoader\ApcClassLoader;
 $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
 */
 
-require_once '/home/gxpvsxmy/Servers/swdestinydb/app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require $root . '/app/AppKernel.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
