1d345a6bca36f810b530fa7262cb1b52df4f748d
OneKatarix <193742323+OneKatarix@users.noreply.github.com>
2026-09-12 19:06:50 +0100

    Removing stale files, un-needed scripts, added some prod specific and general optimisations.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -30,16 +30,19 @@ as_www "php app/console doctrine:schema:update --force --env=${SYMFONY_ENV} --no
     as_www "php -d memory_limit=-1 app/console app:import:std /var/www/html/dbJSON --env=${SYMFONY_ENV} --no-debug"
   fi
 
-if [ "${SYMFONY_ENV}" = "dev" ]; then
-    as_www "php app/console fos:user:create dev dev@localhost dev --env=${SYMFONY_ENV} -n" || true
-    as_www "php app/console fos:user:activate dev --env=${SYMFONY_ENV} -n" || true
-    as_www "php app/console fos:user:promote --super dev --env=${SYMFONY_ENV} -n" || true
-fi
-
 # Manually patch php7.3-4 regressions (https://github.com/doctrine/orm/issues/7402)
 as_www "composer install --no-interaction --prefer-dist --no-scripts"
 as_www "sed -i '2636s/continue;/break;/' /var/www/html/vendor/doctrine/orm/lib/Doctrine/ORM/UnitOfWork.php"
 as_www "sed -i '2665s/continue;/break;/' /var/www/html/vendor/doctrine/orm/lib/Doctrine/ORM/UnitOfWork.php"
-as_www "composer install --no-interaction --prefer-dist"
+
+if [ "${SYMFONY_ENV}" = "dev" ]; then
+    as_www "php app/console fos:user:create dev dev@localhost dev --env=${SYMFONY_ENV} -n" || true
+    as_www "php app/console fos:user:activate dev --env=${SYMFONY_ENV} -n" || true
+    as_www "php app/console fos:user:promote --super dev --env=${SYMFONY_ENV} -n" || true
+	as_www "composer install --no-interaction --prefer-dist"
+fi
+
+# Prod optimizations over dev
+if [ "${SYMFONY_ENV}" = "prod" ]; then as_www "composer install --no-interaction --prefer-dist --optimize-autoloader"; as_www "php -d memory_limit=-1 app/console cache:warmup --env=prod --no-debug"; fi
 
 exec "$@"
