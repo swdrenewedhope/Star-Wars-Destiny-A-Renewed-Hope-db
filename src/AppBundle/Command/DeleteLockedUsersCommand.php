f93ab96fc6ba10f08b724dd3ff059b89e548d7f5
OneKatarix <193742323+OneKatarix@users.noreply.github.com>
2026-09-13 16:40:33 +0100

    Deleted unnecessary files, repaired 'DeleteInactive/Locked Users'.

--- src/AppBundle/Command/DeleteLockedUsersCommand.php
+++ src/AppBundle/Command/DeleteLockedUsersCommand.php
@@ -7,14 +7,14 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
-class DeleteInactiveCommand extends ContainerAwareCommand
+class DeleteLockedUsersCommand extends ContainerAwareCommand
 {
 
     protected function configure()
     {
         $this
-        ->setName('app:inactive-users')
-        ->setDescription('Delete users inactive since 48 hours')
+        ->setName('app:delete-unverified')
+        ->setDescription('Delete users who have not confirmed email after signing >week.')
         ;
     }
 
@@ -22,17 +22,19 @@ class DeleteInactiveCommand extends ContainerAwareCommand
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
         $limit = new \DateTime();
-        $limit->sub(new \DateInterval('PT48H'));
+        $limit->sub(new \DateInterval('P7D'));
         $count = 0;
 
         $users = $em->getRepository('AppBundle:User')->findBy(array('enabled' => false));
+
         foreach($users as $user) {
-            if($user->getDateCreation() < $limit) {
-                $count++;
-                $em->remove($user);
+            if($user->getDateCreation() < $limit) { $count++;
+				$collection = $em->getRepository('AppBundle:Collection')->findOneBy(array('user' => $user));
+				$em->remove($collection);
+				$em->remove($user);
             }
         }
         $em->flush();
-        $output->writeln(date('c') . " Delete $count inactive users.");
+        $output->writeln(date('c') . " Deleted $count garbage accounts.");
     }
 }
