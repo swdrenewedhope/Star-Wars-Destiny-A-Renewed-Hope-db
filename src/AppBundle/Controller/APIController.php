a07b81b3c63f622636d41cafcee5e4b5539c4455
OneKatarix <193742323+OneKatarix@users.noreply.github.com>
2026-09-26 11:37:15 +0100

    Refactored APIController.php

--- src/AppBundle/Controller/APIController.php
+++ src/AppBundle/Controller/APIController.php
@@ -11,37 +11,24 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class APIController extends Controller
 {
-
-
-	# PUBLIC API FUNCTIONS
-
-
-
     function showDocsAction () { return $this->render('AppBundle:API:API.html.twig', [], new Response()); }
 
 	public function listFormatsAction(Request $request)
 	{
-		$response = new Response('', 200, array('Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json'));
-		$list_formats = $this->getDoctrine()->getRepository('AppBundle:Format')->findAll(); $formats = array();
-
-		foreach($list_formats as $format) {
-			$formats[] = array(
-					"name" => $format->getName(),
-					"code" => $format->getCode(),
-					"data" => $format->getData()
-			);
-		}
-
-		$content = json_encode($formats);	
-		$response->setContent($content);
-		return $response;
+		$response = (new Response('', 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json']))->setPublic()->setMaxAge($this->container->getParameter('cache_expiration'));	
+		$list_formats = $this->getDoctrine()->getRepository('AppBundle:Format')->findAll();
+		$lastModified = max(array_map(function ($format) { return $format->getDateUpdate(); }, $list_formats));
+		$response->setLastModified($lastModified); if ($response->isNotModified($request)) { return $response; }
+		foreach($list_formats as $format) { $formats[] = array( "name" => $format->getName(), "code" => $format->getCode(), "data" => $format->getData()); }
+		$content = json_encode($formats); $response->setContent($content); return $response;
 	}
 
 	public function listSetsAction(Request $request)
 	{
-		$response = new Response('', 200, array('Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json'));
+		$response = (new Response('', 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json']))->setPublic()->setMaxAge($this->container->getParameter('cache_expiration'));	
 		$list_sets = $this->getDoctrine()->getRepository('AppBundle:Set')->findAll(); $sets = array();
-
+		$lastModified = max(array_map(function ($set) { return $set->getDateUpdate(); }, $list_sets));
+		$response->setLastModified($lastModified); if ($response->isNotModified($request)) { return $response; }
 		foreach($list_sets as $set) {
 			$real = count($set->getCards());
 			$max = $set->getSize();
@@ -53,88 +40,71 @@ class APIController extends Controller
 					"known" => intval($real),
 					"total" => $max,
 					"url" => $this->get('router')->generate('cards_list', array('set_code' => $set->getCode()), UrlGeneratorInterface::ABSOLUTE_URL),
-			);
-		}
-
-		$content = json_encode($sets);
-		$response->setContent($content);
-		return $response;
+			);} $content = json_encode($sets); $response->setContent($content); return $response;
 	}
 
 	public function getCardAction($card_code, Request $request)
 	{
-		$response = new Response('', 200, array('Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json'));
-		$cardLookup = $this->getDoctrine()->getRepository('AppBundle:Card')->findOneBy(array("code" => $card_code));
-		if(!$cardLookup) { $response -> setStatusCode(404); return $response; }
-		$cardData = $this->get('cards_data')->getCardInfo($cardLookup, true, "en");
-		$content = json_encode($cardData);	
-		$response->setContent($content);
-		return $response;
+		$response = (new Response('', 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json']))->setPublic()->setMaxAge($this->container->getParameter('cache_expiration'));	
+		$card = $this->getDoctrine()->getRepository('AppBundle:Card')->findOneBy(array("code" => $card_code));
+		if(!$card) { $response -> setStatusCode(404); return $response; }
+		$response->setLastModified($card->getDateUpdate()); if ($response->isNotModified($request)) { return $response; }
+		$cardData = $this->get('cards_data')->getCardInfo($card, true, "en");
+		$content = json_encode($cardData); $response->setContent($content); return $response;
 	}
 
 	public function listAllCardsAction(Request $request)
 	{
-		$response = new Response('', 200, array('Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json'));
+		$response = (new Response('', 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json']))->setPublic()->setMaxAge($this->container->getParameter('cache_expiration'));	
 		$list_cards = $this->getDoctrine()->getRepository('AppBundle:Card')->findAll(); $cards = array();
+		$lastModified = max(array_map(function ($card) { return $card->getDateUpdate(); }, $list_cards));
+		$response->setLastModified($lastModified); if ($response->isNotModified($request)) { return $response; }
 		foreach($list_cards as $card) { $cards[] = $this->get('cards_data')->getCardInfo($card, true, 'en'); }
-		$content = json_encode($cards);
-		$response->setContent($content);
-		return $response;
+		$content = json_encode($cards); $response->setContent($content); return $response;
 	}
 
 	public function listCardsBySetAction($set_code, Request $request)
 	{
-		$response = new Response('', 200, array('Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json'));
+		$response = (new Response('', 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json']))->setPublic()->setMaxAge($this->container->getParameter('cache_expiration'));	
 		$set = $this->getDoctrine()->getRepository('AppBundle:Set')->findOneBy(array('code' => $set_code));
 		if (!$set) { $response -> setStatusCode(404); return $response; }
 		$conditions = $this->get('cards_data')->syntax("s:$set_code");
 		$this->get('cards_data')->validateConditions($conditions);
-		$query = $this->get('cards_data')->buildQueryFromConditions($conditions);
-		$cards = array();
-
+		$query = $this->get('cards_data')->buildQueryFromConditions($conditions); $cards = array();
 		if($query && $rows = $this->get('cards_data')->get_search_rows($conditions, "set"))
 		{
+			$lastModified = max(array_map(function ($card) { return $card->getDateUpdate(); }, $rows));
+			$response->setLastModified($lastModified); if ($response->isNotModified($request)) { return $response; }
 			for($rowindex = 0; $rowindex < count($rows); $rowindex++) {
-				$card = $this->get('cards_data')->getCardInfo($rows[$rowindex], true, "en");
-				$cards[] = $card;
+				$card = $this->get('cards_data')->getCardInfo($rows[$rowindex], true, "en"); $cards[] = $card;
 			}
-		}
-
-		$content = json_encode($cards);
-		$response->setContent($content);
-		return $response;
+		} $content = json_encode($cards); $response->setContent($content); return $response;
 	}
 
 	public function findCardsAction(Request $request)
 	{
-		$response = new Response('', 200, array('Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json'));
-		$q = $request->query->get('q');
-		$conditions = $this->get('cards_data')->syntax($q);
+		$response = (new Response('', 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json']))->setPublic()->setMaxAge($this->container->getParameter('cache_expiration'));	
+		$q = $request->query->get('q'); $conditions = $this->get('cards_data')->syntax($q);
 		$this->get('cards_data')->validateConditions($conditions);
-		$query = $this->get('cards_data')->buildQueryFromConditions($conditions);
-		$cards = array();
-
+		$query = $this->get('cards_data')->buildQueryFromConditions($conditions); $cards = array();
 		if($query && $rows = $this->get('cards_data')->get_search_rows($conditions, "set"))
-		{
+		{ $response->setLastModified($this->get('cards_data')->getRowsLastModified($rows));
+        	if ($response->isNotModified($request)) { return $response; }
+
 			for($rowindex = 0; $rowindex < count($rows); $rowindex++) {
 				$card = $this->get('cards_data')->getCardInfo($rows[$rowindex], true, 'en');
 				$cards[] = $card;
 			}
-		}
-
-		$content = json_encode($cards);
-		$response->setContent($content);
-		return $response;
+		} $content = json_encode($cards); $response->setContent($content); return $response;
 	}
 
 	public function getDecklistAction($decklist_id, Request $request)
 	{
-		$response = new Response('', 200, array('Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json'));
+		$response = (new Response('', 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'application/json']))->setPublic()->setMaxAge($this->container->getParameter('cache_expiration'));	
 		$decklist = $this->getDoctrine()->getRepository('AppBundle:Decklist')->find($decklist_id);
 		if(!$decklist) { $response -> setStatusCode(404); return $response; }
-		$content = json_encode($decklist);	
-		$response->setContent($content);
-		return $response;
+		$response->setLastModified($decklist->getDateUpdate()); if ($response->isNotModified($request)) { return $response; }
+		$content = json_encode($decklist); $response->setContent($content); return $response;
 	}
 
 	public function listDecklistsByDateAction($date, Request $request)
@@ -144,9 +114,7 @@ class APIController extends Controller
 		$expr = Criteria::expr();
 		$criteria = Criteria::create()->where($expr->gte('dateCreation', $start))->andWhere($expr->lt('dateCreation', $end));
 		$decklists = iterator_to_array($this->getDoctrine()->getRepository('AppBundle:Decklist')->matching($criteria));
-		$content = json_encode($decklists);
-		$response->setContent($content);
-		return $response;
+		$content = json_encode($decklists); $response->setContent($content); return $response;
 	}
 
 	public function getDeckAction($deck_id, Request $request)
@@ -156,18 +124,13 @@ class APIController extends Controller
     	if (!$deck) { $response -> setStatusCode(404); return $response; }
 		$isOwner = $this->getUser() && $deck->getUser()->getId() === $this->getUser()->getId();
 		if (!$deck->getUser()->getIsShareDecks() && !$isOwner) { $response -> setStatusCode(403); return $response; };
-		$response->setLastModified($deck->getDateUpdate());
-		if ($response->isNotModified($request)) { return $response; }
-		$content = json_encode($deck);
-		$response->setContent($content);
-    	return $response;
+		$response->setLastModified($deck->getDateUpdate()); if ($response->isNotModified($request)) { return $response; }
+		$content = json_encode($deck); $response->setContent($content); return $response;
 	}
 
 
 
 	# OAUTH API FUNCTIONS
-
-
 
 	public function listDecksAction(Request $request)
 	{
@@ -177,9 +140,7 @@ class APIController extends Controller
 		$dateUpdates = array_map(function ($deck) { return $deck->getDateUpdate(); }, $decks);
 		$response->setLastModified(max($dateUpdates));
 		if ($response->isNotModified($request)) { return $response; }
-		$content = json_encode($decks);
-		$response->setContent($content);
-		return $response;
+		$content = json_encode($decks); $response->setContent($content); return $response;
 	}
 	
 	public function saveDeckAction($id, Request $request)
